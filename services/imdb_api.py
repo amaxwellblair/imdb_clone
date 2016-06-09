fe6b86c7337e7e375d1c93cf355df38b23e43e83
Allan <amaxwellblair@gmail.com>
2016-06-09 10:41:15 -0700

    Create IMDB service and structure webpack

---
+++ services/imdb_api.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class MovieApi:
+    def __init__(self):
+        self.url = 'http://www.omdbapi.com/?'
+
+    def get_movie_search(self, movie):
+        params = 't=' + movie
+        r = requests.get(self.url + params)
+        print r.content
+
+x = MovieApi()
+x.get_movie_search('Lion King')
--- webpack/index.js
+++ webpack/index.js
@@ -1,1 +1,12 @@
+const React = require('react');
+const { render } = require('react-dom');
 
+class App extends React.Component {
+  render() {
+    return (
+      <p>Hello World</p>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));

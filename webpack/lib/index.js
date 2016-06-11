dded93249d45a5ab34376fe23bd90328d1092007
Allan <amaxwellblair@gmail.com>
2016-06-11 14:25:44 -0700

    Create remove button

--- webpack/lib/index.js
+++ webpack/lib/index.js
@@ -6,9 +6,50 @@ const EventEmitter = require('events');
 const model = new MovieModel();
 const emitter = new EventEmitter();
 
-function movieBox(props) {
+function saveButton(props) {
+  const handleClick = () => {
+    emitter.emit('save', props);
+  };
+
+  if (props.Poster) {
+    return (
+      <div className='card-content'>
+        <button className='btn' onClick={ handleClick }>Save</button>
+      </div>
+    );
+  }
+  return '';
+}
+
+function deleteButton(props) {
+  const handleClick = () => {
+    emitter.emit('delete', props);
+  };
+
+  return (
+    <a onClick={ handleClick } className='close'>{ '\u2715' }</a>
+  );
+}
+
+function movieSearchBox(props) {
   return (
     <div className='card'>
+      <div className='card-image'>
+        <img src={ props.Poster }></img>
+      </div>
+      <div className='card-content'>
+        <span className='card-title'>{ props.Title }</span>
+        <p className='card-text'>{ props.Plot }</p>
+      </div>
+      { saveButton(props) }
+    </div>
+  );
+}
+
+function movieBox(props) {
+  return (
+    <div className='card store-movie' key={ props.imdbID }>
+      { deleteButton(props) }
       <div className='card-image'>
         <img src={ props.Poster }></img>
       </div>
@@ -24,7 +65,48 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <MovieSearch/>
+        <div className='row'>
+          <MovieSearch/>
+          <MovieStore/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class MovieStore extends React.Component {
+  constructor() {
+    super();
+    this.state = { movies: [] };
+    this.saveListener((props) => {
+      const movies = this.state.movies;
+      movies.push(movieBox(props));
+      this.setState({ movies });
+    });
+    this.deleteListener((props) => {
+      const movies = this.state.movies;
+      for (let i = 0; i < movies.length; i++) {
+        if (movies[i].key === props.imdbID) {
+          movies.splice(i, 1);
+          break;
+        }
+      }
+      this.setState({ movies });
+    });
+  }
+
+  saveListener(callback) {
+    emitter.on('save', callback);
+  }
+
+  deleteListener(callback) {
+    emitter.on('delete', callback);
+  }
+
+  render() {
+    return (
+      <div className='col m8'>
+        { this.state.movies }
       </div>
     );
   }
@@ -54,13 +136,9 @@ class MovieSearch extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className='row'>
-          <div className='col m4' id='side-bar'>
-            <input id='search' placeholder='Search...' onKeyUp={this.pressSearch} />
-            { movieBox(this.state.movie) }
-          </div>
-        </div>
+      <div className='col m4' id='side-bar'>
+        <input id='search' placeholder='Search...' onKeyUp={this.pressSearch} />
+        { movieSearchBox(this.state.movie) }
       </div>
     );
   }
